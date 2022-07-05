369791e4c6897c568565d50338d73707c94c94ef
Jafet Solano <youremail@domain.com>
2022-07-05 01:06:58 -0600

    first commit

---
+++ src/components/charactersList.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+import Character from './character';
+
+function NavPage(props){
+    return (
+        <header className='d-flex justify-content-between aling=items-center'>
+            <p>Page: {props.page}</p>
+            <button 
+                className='btn btn-primary btn-sm'
+                onClick={() => props.setPage(props.page + 1)}
+                >
+                Page: {props.page + 1}</button>
+        </header>
+    )
+}
+
+function charactersList() {
+    const [characters, setCharacters] = useState([])
+    const [page, setPage] = useState(1)
+
+    useEffect(() => {
+    async function fetchData(){
+      const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
+      const data = await response.json();
+      setCharacters(data.results);
+    }
+    fetchData()
+    },[page]);
+
+  return (
+    <div className='container' >
+        <NavPage page={page} setPage={setPage}/>
+        <div className='row'>
+            {
+                characters.map((character) => {
+                    return(
+                        <div className='col-md-4' key={character.id}>
+                            <Character character={character}/>
+                        </div>
+                    )
+                })
+            }
+        </div>
+        <NavPage page={page} setPage={setPage}/>
+    </div>
+  )
+}
+
+export default charactersList
